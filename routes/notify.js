ade814bc0162ae56caa3614972fca636a5cb3bf0
edmoz <edwong@mozilla.com>
2015-10-20 15:46:35 -0700

    add repeat and delay params

--- routes/notify.js
+++ routes/notify.js
@@ -9,17 +9,37 @@ module.exports = {
     handler: function (req, reply) {
       console.log('got: ' + JSON.stringify(req.payload) );
       var params = req.payload;
+      var count = 1;
 
-      webpush.sendNotification(
-        params.endpoint,
-        params.TTL,
-        params.userPublicKey,
-        params.payload
-      ).then(function (result) {
-        reply(result);
-      }).catch(function (err) {
-        reply(err);
-      });
+      function doSend(){
+        webpush.sendNotification(
+            params.endpoint,
+            params.TTL,
+            params.userPublicKey,
+            new Buffer(params.payload, 'utf8')
+          ).then(function (result) {
+            reply(result);
+          }).catch(function (err) {
+            reply(err);
+          });
+      }
+      // send a notification right away
+      doSend();
+
+      //repeat
+      if ( params.repeat > 1){
+        var interval = setInterval(function() {
+          // console.log('count: '+count);
+          doSend();
+          if ( count >= params.repeat - 1){
+              clearInterval(interval);
+          }
+          count++;
+        }, params.delay);
+      }
+
+
+
     }
   }
 };
