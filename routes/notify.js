037343dd4afe59f8957f935340ba0d80ba635213
edmoz <edwong@mozilla.com>
2015-10-23 16:20:57 -0700

    support chrome

--- routes/notify.js
+++ routes/notify.js
@@ -7,27 +7,37 @@ module.exports = {
   path: '/notify',
   config: {
     handler: function (req, reply) {
+      console.log('ua:' + JSON.stringify(req.headers));
       console.log('got: ' + JSON.stringify(req.payload) );
+
+      if (!process.env.GCM_API_KEY) {
+        console.error('If you want Chrome to work, you need to set the GCM_API_KEY environment variable to your GCM API key.');
+      }
+
       var params = req.payload;
       var count = 1;
+      var repeat = params.repeat || 1;
+      var delay = params.delay || 15;
 
       function doSend(){
-        webpush.sendNotification(
-            params.endpoint,
-            params.TTL,
-            params.userPublicKey,
-            new Buffer(params.payload, 'utf8')
-          ).then(function (result) {
-            reply(result);
+        var promise;
+        webpush.setGCMAPIKey(process.env.GCM_API_KEY);
+        if (params.payload){
+          promise = webpush.sendNotification(params.endpoint, params.TTL, userPublicKey, new Buffer(payload, 'utf8'));
+        }else{
+          promise = webpush.sendNotification(params.endpoint, params.TTL);
+        }
+        promise.then(function (result) {
+            console.log(result);
           }).catch(function (err) {
-            reply(err);
+            console.log(err);
           });
       }
       // send a notification right away
       doSend();
 
       //repeat
-      if ( params.repeat > 1){
+      if ( repeat > 1){
         var interval = setInterval(function() {
           // console.log('count: '+count);
           doSend();
@@ -35,7 +45,7 @@ module.exports = {
               clearInterval(interval);
           }
           count++;
-        }, params.delay);
+        }, delay);
       }
 
 
